195dafc1a1bdd1ba1e825c3538bd813021fcdd70
GiovanePS <giovanespace@gmail.com>
2024-02-15 02:39:33 -0300

    ✨ feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/GiovanePS/go-api-study/router"
+import (
+	"github.com/GiovanePS/go-api-study/config"
+	"github.com/GiovanePS/go-api-study/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
