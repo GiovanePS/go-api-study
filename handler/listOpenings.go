439f9adb5b6945e0792808ed1604907f3b9eab5c
GiovanePS <giovanespace@gmail.com>
2024-02-18 00:56:21 -0300

    :sparkles: feat: list openings

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -3,11 +3,17 @@ package handler
 import (
 	"net/http"
 
+	"github.com/GiovanePS/go-api-study/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningsHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"message": "GET All Openings",
-	})
+	openings := schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(context, http.StatusInternalServerError, "error on listing openings")
+		return
+	}
+
+	sendSuccess(context, "list openings", openings)
 }
