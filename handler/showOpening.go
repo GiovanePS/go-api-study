dc0be2a7ec315aa125df31a7d6332976724a1038
GiovanePS <giovanespace@gmail.com>
2024-02-18 01:26:45 -0300

    :sparkles: feat: show opening

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -1,13 +1,27 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/GiovanePS/go-api-study/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"message": "GET Opening",
-	})
+	id := context.Query("id")
+
+	if id == "" {
+		sendError(context, http.StatusBadRequest, errParamIsRequired("id", "queryParam").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(context, http.StatusNotFound, fmt.Sprintf("opening with id %s not found", id))
+		return
+	}
+
+	sendSuccess(context, "show opening", opening)
 }
